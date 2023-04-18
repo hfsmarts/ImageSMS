fddcabb7a483a828bfa8cb0bf853f17b35567f37
hfsmarts <harunfazlic@yahoo.com>
2023-04-18 18:19:22 +0200

    upload verifying

--- ImageSMS/ViewController.swift
+++ ImageSMS/ViewController.swift
@@ -18,8 +18,16 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     @IBOutlet var namePlaceholder: UITextField!
     @IBOutlet var messagePlaceholder: UITextField!
     @IBOutlet var sendButton: UIButton!
+    var uploadedImageURL: String? = nil
     
-    var uploadedImageURL: String?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.hideKeyboardWhenTappedAround()
+        UtilityFunctions().setPlaceHolderColor(placeHolderText: "38761222111", placeHolder: numberPlaceHolder)
+        UtilityFunctions().setPlaceHolderColor(placeHolderText: "Mujo Mujic", placeHolder: namePlaceholder)
+        UtilityFunctions().setPlaceHolderColor(placeHolderText: "Wish you all the best!", placeHolder: messagePlaceholder)
+        sendButton.isEnabled = false
+    }
     
     func uploadImageToImgur(image: UIImage, completion: @escaping (Result<String, Error>) -> Void)  {
         guard let imageData = image.jpegData(compressionQuality: 0.8) else {
@@ -27,9 +35,8 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             return
         }
         
-        
         let headers: HTTPHeaders = [
-            "Authorization": UtilityFunctions().keyGenerator(key: "ClientID") /*Client ID goes here*/
+            "Authorization": UtilityFunctions().keyGenerator(key: "ClientID")
         ]
         
         AF.upload(multipartFormData: { multipartFormData in
@@ -40,28 +47,10 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             switch response.result {
             case .success(let uploadResponse):
                 completion(.success(uploadResponse.data.link))
-                
-               
-                
             case .failure(let error):
                 completion(.failure(error))
             }
         }
-    }
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.hideKeyboardWhenTappedAround()
-        UtilityFunctions().setPlaceHolderColor(placeHolderText: "38761222111", placeHolder: numberPlaceHolder)
-        UtilityFunctions().setPlaceHolderColor(placeHolderText: "Mujo Mujic", placeHolder: namePlaceholder)
-        UtilityFunctions().setPlaceHolderColor(placeHolderText: "Wish you all the best!", placeHolder: messagePlaceholder)
-        sendButton.isEnabled = false
-        
-    }
-    
-    @IBAction func textFieldEditingChanged(_ sender: Any) {
-        sendButton.isEnabled = !numberPlaceHolder.text!.isEmpty && !namePlaceholder.text!.isEmpty && !messagePlaceholder.text!.isEmpty
     }
     
     @IBAction func browseForTheImage(_ sender: UIButton) {
@@ -70,68 +59,63 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         vc.delegate = self
         vc.allowsEditing = true
         present(vc, animated: true)
-        
     }
     
     @IBAction func sendMessage(_ sender: UIButton) {
         
-        //& uploaded image != nil
-                    
-            let accountSid = UtilityFunctions().keyGenerator(key: "accountSid")
-            let authToken = UtilityFunctions().keyGenerator(key: "authToken")
-            let urlString = "https://api.twilio.com/2010-04-01/Accounts/\(accountSid)/Messages.json"
-            let body = "Message from: \(namePlaceholder.text!). Message text: \(messagePlaceholder.text!) "
-            let from = "+15673991807"
-            let to = "+\(numberPlaceHolder.text!)"
-            let parameters: [String: Any] = [
-                "Body": body,
-                "From": from,
-                "To": to,
-                "MediaUrl": uploadedImageURL!
-            ]
-
-
-            AF.request(urlString, method: .post, parameters: parameters, encoding: URLEncoding.default, headers: HTTPHeaders(["Authorization": "Basic \(Data("\(accountSid):\(authToken)".utf8).base64EncodedString())"])).response { response in
-                if let error = response.error {
-                    UtilityFunctions().errorAlert(vc: self)
-                    print("Error: \(error.localizedDescription)")
-                    return
-                }
-
-                guard let statusCode = response.response?.statusCode,
-                      (200...299).contains(statusCode) else {
-                    UtilityFunctions().errorAlert(vc: self)
-                    print("Error: Invalid response")
-                    return
-                }
-
-                guard let data = response.data else {
-                    UtilityFunctions().errorAlert(vc: self)
-                    print("Error: No data received")
-                    return
-                }
-
-                UtilityFunctions().success(vc: self)
-                print(String(data: data, encoding: .utf8)!)
+        let accountSid = UtilityFunctions().keyGenerator(key: "accountSid")
+        let authToken = UtilityFunctions().keyGenerator(key: "authToken")
+        let urlString = "https://api.twilio.com/2010-04-01/Accounts/\(accountSid)/Messages.json"
+        let body = "Message from: \(namePlaceholder.text!). Message text: \(messagePlaceholder.text!) "
+        let from = "+15673991807"
+        let to = "+\(numberPlaceHolder.text!)"
+        let parameters: [String: Any] = [
+            "Body": body,
+            "From": from,
+            "To": to,
+            "MediaUrl": uploadedImageURL!
+        ]
+        
+        AF.request(urlString, method: .post, parameters: parameters, encoding: URLEncoding.default, headers: HTTPHeaders(["Authorization": "Basic \(Data("\(accountSid):\(authToken)".utf8).base64EncodedString())"])).response { response in
+            if let error = response.error {
+                UtilityFunctions().errorAlert(vc: self)
+                print("Error: \(error.localizedDescription)")
+                return
             }
+            
+            guard let statusCode = response.response?.statusCode,
+                  (200...299).contains(statusCode) else {
+                UtilityFunctions().errorAlert(vc: self)
+                print("Error: Invalid response")
+                return
+            }
+            
+            guard let data = response.data else {
+                UtilityFunctions().errorAlert(vc: self)
+                print("Error: No data received")
+                return
+            }
+            
+            UtilityFunctions().success(vc: self)
+            print(String(data: data, encoding: .utf8)!)
         }
-    
-    
+    }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let image = info[UIImagePickerController.InfoKey(rawValue: "UIImagePickerControllerEditedImage")] as? UIImage {
             imageInMessage.image = image
-            
             uploadImageToImgur(image: image) { result in
                 switch result {
                 case .success(let imageURL):
-                    print("Image uploaded successfully: \(imageURL)")
-                    self.uploadedImageURL = imageURL // set the uploaded image URL to the property
+                    self.uploadedImageURL = imageURL
+                    if self.numberPlaceHolder.text! != "" && self.namePlaceholder.text! != "" && self.messagePlaceholder.text! != "" {
+                        self.sendButton.isEnabled = true
+                    }
                 case .failure(let error):
                     print("Failed to upload image: \(error.localizedDescription)")
+                    self.sendButton.isEnabled = false
                 }
             }
-            
             picker.dismiss(animated: true)
         }
         
@@ -140,12 +124,9 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         }
     }
     
-    
-    
-    
 }
 
 
-    
+
 
 
